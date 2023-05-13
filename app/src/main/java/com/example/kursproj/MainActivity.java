4d786df665d7d6c82e4b85f735ddaf6282f1e7de
Elizaveta Telnova <bubblekvas@outlook.com>
2023-05-14 02:12:21 +0300

    +

--- app/src/main/java/com/example/kursproj/MainActivity.java
+++ app/src/main/java/com/example/kursproj/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.kursproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
