86a95029a9894a8f804d52f31965b9f8085720fc
idalynnn <eliz.telnova@gmail.com>
2023-06-05 02:54:55 +0300

    + diary

--- app/src/main/java/com/example/kursproj/MainActivity.java
+++ app/src/main/java/com/example/kursproj/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         ImageButton goToAllNotes = (ImageButton)findViewById(R.id.notesButton);
         ImageButton goToSettings = (ImageButton)findViewById(R.id.settingsButton);
         ImageButton goToInfo = (ImageButton)findViewById(R.id.infoButton);
+        ImageButton goToDiary = (ImageButton)findViewById(R.id.dailyButton);
 
         goToAllNotes.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,10 +33,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        goToInfo.setOnClickListener(new View.OnClickListener() {
+       /* goToInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
                 startActivity(new Intent(MainActivity.this, AllNotes.class));
+            }
+        });*/
+
+        goToInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, Diary.class));
             }
         });
 
